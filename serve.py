d7bd4ee7fa07127d2ba33d60c0b4220f43a547e6
Jason R. Coombs <jaraco@jaraco.com>
2016-04-10 11:23:17 +0200

    Add config such that server port may be indicated in environment.

--- serve.py
+++ serve.py
@@ -1,5 +1,18 @@
+import os
+
 import cherrypy
 
+
+config = {
+	'global': {
+		'server.production': True,
+		'server.socket_port': int(os.environ.get('PORT', 8080)),
+		'server.socket_host': '::1',
+		'tools.encode.on': True,
+		'tools.encode.encoding': 'utf-8',
+		'tools.decode.on': True,
+	},
+}
 
 class Server:
 	@cherrypy.expose
@@ -8,7 +21,7 @@ class Server:
 
 	@classmethod
 	def run(cls):
-		cherrypy.quickstart(cls())
+		cherrypy.quickstart(cls(), config=config)
 
 
 if __name__ == '__main__':
